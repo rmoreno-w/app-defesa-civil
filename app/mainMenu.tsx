0f8653a17da0a8fc65bfeeaa6abe6241ca5cc2ff
rmoreno-w <rmoreno.w@gmail.com>
2023-06-16 14:26:32 -0300

    🚀 Main Menu Page

---
+++ app/mainMenu.tsx
@@ -0,0 +1,59 @@
+import { StyleSheet, Text, View } from 'react-native';
+import MainMenuOption from '../src/components/MainMenuOption';
+import NewsCard from '../src/components/NewsCard';
+import colors from '../src/styles/colors';
+import fonts from '../src/styles/fonts';
+
+export default function Page() {
+    function onButtonPress() {}
+
+    return (
+        <View style={styles.container}>
+            <Text style={styles.title}>SisVil</Text>
+
+            <View style={styles.menuContainer}>
+                <Text style={styles.subtitle}>Main Menu</Text>
+                <View style={styles.optionsContainer}>
+                    <MainMenuOption option='Consultar meus incidentes' icon='list' />
+                    <MainMenuOption option='Reportar incidente' icon='cloud' />
+                </View>
+
+                <Text>Últimas notícias:</Text>
+                <NewsCard />
+            </View>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        paddingVertical: 48,
+        paddingHorizontal: 16,
+        backgroundColor: colors.blue_50,
+    },
+    title: {
+        // backgroundColor: 'green',
+        fontFamily: fonts.heading,
+        fontSize: 56,
+        textAlign: 'center',
+        color: colors.blue_800,
+    },
+    subtitle: {
+        fontFamily: fonts.text,
+        fontSize: 16,
+        color: colors.blue_900,
+        textAlign: 'center',
+        // backgroundColor: 'red',
+    },
+    menuContainer: {
+        gap: 24,
+    },
+    optionsContainer: {
+        // backgroundColor: 'red',
+        flexDirection: 'row',
+        gap: 24,
+        flexWrap: 'wrap',
+        justifyContent: 'space-between',
+    },
+});
