d94871f9dea77897e49a23d757462b1806156b9c
rmoreno-w <rmoreno.w@gmail.com>
2023-06-16 14:23:03 -0300

    🚀 Login Page

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,19 @@
 import { Pridi_700Bold } from '@expo-google-fonts/pridi';
 import { WorkSans_400Regular, useFonts } from '@expo-google-fonts/work-sans';
+import { useRouter } from 'expo-router';
+import { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { TouchableOpacity } from 'react-native-gesture-handler';
+import Input from '../src/components/Input';
+import colors from '../src/styles/colors';
 import fonts from '../src/styles/fonts';
 
-export default function Page() {
-    let [fontsLoaded] = useFonts({
+export default function loginPageLayout() {
+    const router = useRouter();
+    const [email, setEmail] = useState('');
+    const [senha, setSenha] = useState('');
+
+    const [fontsLoaded] = useFonts({
         WorkSans_400Regular,
         Pridi_700Bold,
     });
@@ -13,11 +22,35 @@ export default function Page() {
         return null;
     }
 
+    function onButtonPress() {
+        router.replace('/mainMenu');
+    }
+
     return (
         <View style={styles.container}>
-            <View style={styles.main}>
-                <Text style={styles.title}>Hello World</Text>
-                <Text style={styles.subtitle}>This is the first page of your app.</Text>
+            <Text style={styles.title}>SisVil</Text>
+
+            {/* Card */}
+            <View style={styles.card}>
+                <Text style={styles.subtitle}>Faça login para conferir as atualizações na sua área!</Text>
+                <View style={styles.form}>
+                    <Input
+                        label='Email'
+                        placeholder='maria@exemplo.com'
+                        inputData={email}
+                        setInputFunction={setEmail}
+                    />
+                    <Input
+                        label='Senha'
+                        placeholder='***********************'
+                        inputData={senha}
+                        setInputFunction={setSenha}
+                        type='password'
+                    />
+                </View>
+                <TouchableOpacity style={styles.button} onPress={onButtonPress}>
+                    <Text style={styles.buttonText}>Login</Text>
+                </TouchableOpacity>
             </View>
         </View>
     );
@@ -26,23 +59,43 @@ export default function Page() {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        alignItems: 'center',
-        padding: 24,
-    },
-    main: {
-        flex: 1,
-        justifyContent: 'center',
-        maxWidth: 960,
-        marginHorizontal: 'auto',
+        paddingVertical: 48,
+        paddingHorizontal: 16,
+        backgroundColor: colors.blue_50,
+        // backgroundColor: 'red',
     },
     title: {
         fontFamily: fonts.heading,
-        fontSize: 64,
-        color: '#2B2C3C',
+        fontSize: 56,
+        textAlign: 'center',
+        color: colors.blue_800,
+    },
+    card: {
+        marginTop: 64,
+        padding: 24,
+        backgroundColor: colors.blue_800,
+        borderRadius: 12,
+        gap: 32,
     },
     subtitle: {
         fontFamily: fonts.text,
-        fontSize: 36,
-        color: '#38434D',
+        fontSize: 16,
+        color: colors.blue_50,
+        textAlign: 'center',
+        // backgroundColor: 'red',
+    },
+    button: {
+        alignContent: 'center',
+        justifyContent: 'center',
+        height: 56,
+        backgroundColor: colors.blue_300,
+        borderRadius: 8,
+    },
+    buttonText: {
+        textAlign: 'center',
+        color: colors.blue_900,
+    },
+    form: {
+        gap: 12,
     },
 });
